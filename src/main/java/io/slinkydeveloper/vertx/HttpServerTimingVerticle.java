fcde6f19ecd27592001ba3b602fa07bea9b339cf
slinkydeveloper <francescoguard@gmail.com>
2019-06-25 18:00:37 +0200

    Added vertx-web
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/slinkydeveloper/vertx/HttpServerTimingVerticle.java
+++ src/main/java/io/slinkydeveloper/vertx/HttpServerTimingVerticle.java
@@ -2,64 +2,60 @@ package io.slinkydeveloper.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class HttpServerTimingVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx
-      .createHttpServer()
-      .requestHandler(this::handleRequest)
-      .listen(8080, ar -> {
-        if (ar.succeeded()) startPromise.complete();
-        else startPromise.fail(ar.cause());
+    Router router = Router.router(vertx);
+
+    router
+      .get("/timing")
+      .handler(routingContext -> {
+        vertx
+          .eventBus()
+          .<JsonArray>request("get_timings.timingsapp", null, ar -> {
+            if (ar.succeeded()) {
+              routingContext
+                .response()
+                .setStatusCode(200)
+                .putHeader("content-type", "application/json")
+                .end(ar.result().body().toBuffer());
+            } else {
+              System.out.println("Error while retrieving stuff: " + ar.cause());
+              routingContext
+                .response()
+                .setStatusCode(500)
+                .end();
+            }
+          });
       });
-  }
 
-  private void handleRequest(HttpServerRequest req) {
-    if (!req.path().equals("/timing")) {
-      req.response().setStatusCode(404).end();
-      return;
-    }
-
-    if (req.method() == HttpMethod.GET) {
-      // Handle GET /timing
-      vertx
-        .eventBus()
-        .<JsonArray>request("get_timings.timingsapp", null, ar -> {
-          if (ar.succeeded()) {
-            req
-              .response()
-              .setStatusCode(200)
-              .putHeader("content-type", "application/json")
-              .end(ar.result().body().toBuffer());
-          } else {
-            System.out.println("Error while retrieving stuff: " + ar.cause());
-            req
-              .response()
-              .setStatusCode(500)
-              .end();
-          }
-        });
-    } else if (req.method() == HttpMethod.POST) {
-      // Handle POST /timing
-      req.bodyHandler(bodyBuf -> {
-        JsonObject obj = bodyBuf.toJsonObject();
+    router
+      .post("/timing")
+      .handler(BodyHandler.create())
+      .handler(routingContext -> {
+        JsonObject obj = routingContext.getBodyAsJson();
         vertx
           .eventBus()
           .send("add_timing.timingsapp", obj);
-        req
+        routingContext
           .response()
           .setStatusCode(202)
           .end();
       });
-    } else {
-      req.response().setStatusCode(405).end();
-    }
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+      .listen(8080, ar -> {
+        if (ar.succeeded()) startPromise.complete();
+        else startPromise.fail(ar.cause());
+      });
   }
 
 }
