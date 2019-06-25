c027056c59a69fb569a072968fff561dbe833a66
slinkydeveloper <francescoguard@gmail.com>
2019-06-25 12:08:48 +0200

    Added http server logic
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/slinkydeveloper/vertx/MainVerticle.java
+++ src/main/java/io/slinkydeveloper/vertx/MainVerticle.java
@@ -1,14 +1,37 @@
 package io.slinkydeveloper.vertx;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerRequest;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start(Future<Void> startFuture) throws Exception {
-    super.start(startFuture);
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx
+        .createHttpServer()
+        .requestHandler(this::handleRequest)
+        .listen(8080, ar -> {
+      if (ar.succeeded()) startPromise.complete();
+      else startPromise.fail(ar.cause());
+    });
+  }
+
+  private void handleRequest(HttpServerRequest req) {
+    if (!req.path().equals("/timing")) {
+      req.response().setStatusCode(404).end();
+      return;
+    }
+
+    if (req.method() == HttpMethod.GET) {
+      // Handle GET /timing
+    } else if (req.method() == HttpMethod.POST) {
+      // Handle POST /timing
+    } else {
+      req.response().setStatusCode(405).end();
+    }
   }
 
   public static void main(String[] args) {
