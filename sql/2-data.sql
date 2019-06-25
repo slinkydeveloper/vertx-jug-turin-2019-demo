97d1f5ae3574212055511541fc9f530dd2fb2272
slinkydeveloper <francescoguard@gmail.com>
2019-06-25 12:29:30 +0200

    Added docker compose and sql files
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

---
+++ sql/2-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO timing.driver (id, name)
+VALUES  (1, 'Lewis Hamilton'),
+        (2, 'Sebastian Vettel'),
+        (3, 'Valteri Bottas'),
+        (4, 'Charles Leclerc'),
+        (5, 'Max Verstappen'),
+        (6, 'Pierre Gasly');
