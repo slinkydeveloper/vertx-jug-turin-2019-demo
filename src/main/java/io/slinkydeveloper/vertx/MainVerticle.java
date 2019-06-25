e40eb4c8856fc7f46479514d4425f7ec27123ddf
slinkydeveloper <francescoguard@gmail.com>
2019-06-25 09:09:26 +0200

    Created MainVerticle
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/slinkydeveloper/vertx/MainVerticle.java
+++ src/main/java/io/slinkydeveloper/vertx/MainVerticle.java
@@ -1,96 +1,14 @@
 package io.slinkydeveloper.vertx;
 
-import io.vertx.core.*;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
-import io.vertx.pgclient.PgConnectOptions;
-import io.vertx.pgclient.PgPool;
-import io.vertx.sqlclient.PoolOptions;
-import io.vertx.sqlclient.Row;
-import io.vertx.sqlclient.Tuple;
-
-import java.time.Duration;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalUnit;
-import java.util.List;
-import java.util.stream.Collector;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private PgPool pgClient;
-
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    // Connect options
-    PgConnectOptions connectOptions = new PgConnectOptions()
-        .setPort(5432)
-        .setHost("localhost")
-        .setDatabase("timing")
-        .setUser("postgres")
-        .setPassword("postgres");
-
-    // Create the client pool
-    this.pgClient = PgPool.pool(connectOptions, new PoolOptions());
-
-    vertx
-        .createHttpServer()
-        .requestHandler(this::handleRequest)
-        .listen(8080, ar -> {
-      if (ar.succeeded()) startPromise.complete();
-      else startPromise.fail(ar.cause());
-    });
-  }
-
-  private void handleRequest(HttpServerRequest req) {
-    if (!req.path().equals("/timing")) {
-      req.response().setStatusCode(404).end();
-      return;
-    }
-
-    if (req.method() == HttpMethod.GET) {
-      // Handle GET /timing
-    } else if (req.method() == HttpMethod.POST) {
-      // Handle POST /timing
-    } else {
-      req.response().setStatusCode(405).end();
-    }
-  }
-
-  private void postTiming(int driver, int lap, String time) {
-    pgClient.preparedQuery(
-        "INSERT INTO timing.time VALUES ($1, $2, $3)",
-        Tuple.of(driver, lap, time),
-        ar -> {
-          if (ar.succeeded())
-            System.out.format("Added timing for driver %d at lap %d with time %s\n", driver, lap, time);
-          else if (ar.failed())
-            System.out.println("Something went wrong! " + ar.cause());
-        }
-    );
-  }
-
-  private void getAllTimings(Handler<AsyncResult<JsonArray>> handler) {
-    pgClient.query(
-        "SELECT time.driver_id, time.lap, time.time FROM timing.time JOIN timing.driver ON driver.id = time.driver_id",
-        Collector.of(
-            JsonArray::new,
-            (ja, row) -> ja.add(
-                new JsonObject()
-                    .put("driver_id", row.getInteger("driver_id"))
-                    .put("lap", row.getInteger("lap"))
-                    .put("time", row.getString("time"))
-            ),
-            JsonArray::addAll
-        ),
-        ar -> {
-      if (ar.failed()) {
-        handler.handle(Future.failedFuture(ar.cause()));
-      } else {
-        handler.handle(Future.succeededFuture(ar.result().value()));
-      }
-    });
+  public void start(Future<Void> startFuture) throws Exception {
+    super.start(startFuture);
   }
 
   public static void main(String[] args) {
